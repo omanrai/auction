59977997548e4a4590cc12ac269a666ac7b5bd14
Oman Rai <oman2023rai@gmail.com>
2025-07-08 13:42:29 +0545

    Refactor auction app: restructure files, implement auction add and list screens, and integrate Firebase services

---
+++ lib/1st phase/main1.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+// import '1st phase/auction_list_screen.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Auction App',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: AuctionListPage(),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'auction_list_screen.dart';
+import '1st phase/auction_list_screen.dart';
 
 void main() {
   runApp(MyApp());
